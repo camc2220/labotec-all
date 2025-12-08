6a7d7a954d5f5dae20a8c375f3768921d7a6042d
camc2220 <camc2220@gmail.com>
2025-12-07 21:51:48 -0400

    updated

---
+++ Labotec.Api/Data/Migrations/20251129152119_AddInvoices.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Labotec.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabOrderItems_LabTests_LabTestId",
+                table: "LabOrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabOrders_Patients_PatientId",
+                table: "LabOrders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Patients",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabOrderItems_LabTests_LabTestId",
+                table: "LabOrderItems",
+                column: "LabTestId",
+                principalTable: "LabTests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabOrders_Patients_PatientId",
+                table: "LabOrders",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabOrderItems_LabTests_LabTestId",
+                table: "LabOrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabOrders_Patients_PatientId",
+                table: "LabOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Patients",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabOrderItems_LabTests_LabTestId",
+                table: "LabOrderItems",
+                column: "LabTestId",
+                principalTable: "LabTests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabOrders_Patients_PatientId",
+                table: "LabOrders",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
